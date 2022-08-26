bdf777d90cb726c070df5c7d49930b0e026e446f
Ng Guoyou <gyng@users.noreply.github.com>
2022-08-27 03:53:05 +0800

    Use cat_server in test_yolov5 as well

--- test/test_yolov5.py
+++ test/test_yolov5.py
@@ -1,8 +1,10 @@
 from app.handlers.yolov5.handler import YoloV5
 
 
+cat_pic = "http://cat_server:8888/cat"
+
+
 def test_infer_yolov5():
-    cat_pic = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/390px-Cat_poster_1.jpg"  # noqa: E501
     handler = YoloV5()
     result = handler.infer(cat_pic)
     assert result.str_repr.startswith("image 1/1:")
